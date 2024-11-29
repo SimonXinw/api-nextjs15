c64c945eaf04ee727ec5255de75b7425742d5cec
xinwang <1294600058@qq.com>
2024-11-29 15:44:21 +0800

    feat: 删除 eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 禁用构建时的 ESLint 检查
+  },
 };
 
 export default nextConfig;
